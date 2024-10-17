9ff5b7c8517689250c7aba14d42a8ee41e0ae334
yassine-bensada <bensada.ya14@gmail.com>
2024-10-17 23:39:02 +0100

    Basic projet configuration setup

---
+++ src/test/java/com/user_management/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.user_management.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
